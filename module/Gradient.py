8db3a66fbf1c672a6982577aedd993a28cf19e76
Hamed Vaseghnia <93613536+hamedvaseghnia@users.noreply.github.com>
2024-04-12 10:37:24 +0200

    Update Gradient.py

--- module/Gradient.py
+++ module/Gradient.py
@@ -10,18 +10,12 @@ class ElevationDataSlopeCalculator:
     def __init__(self, elevation_data):
         """
         Initializes the calculator with loaded elevation data.
-        
-        Parameters:
-        - elevation_data: Array-like, the elevation data to calculate slope from.
         """
         self.elevation_data = elevation_data
 
     def calculate_slope(self):
         """
         Calculates the slope from the elevation data.
-        
-        Returns:
-        - slope_degrees: A 2D numpy array of the slope in degrees.
         """
         gradient_x, gradient_y = np.gradient(self.elevation_data)
         slope = np.sqrt(gradient_x**2 + gradient_y**2)
@@ -30,10 +24,7 @@ class ElevationDataSlopeCalculator:
     
     def visualize_slope(self, slope_degrees):
         """
-        Visualizes the calculated slope in degrees using a matplotlib plot.
-        
-        Parameters:
-        - slope_degrees: A 2D numpy array of the slope in degrees.
+        Visualizes the calculated slope in degrees.
         """
         fig, ax = plt.subplots(figsize=(10, 6))
         slope_mappable = ax.imshow(slope_degrees, cmap='pink', aspect='auto')
