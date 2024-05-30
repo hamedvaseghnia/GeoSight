c90c3222d875e2fddb84a5438857e3f5d8020de3
Hamed Vaseghnia <93613536+hamedvaseghnia@users.noreply.github.com>
2024-05-30 09:01:29 +0200

    Add files via upload

--- GeoSight/bin/Visualize.py
+++ GeoSight/bin/Visualize.py
@@ -5,6 +5,12 @@ def main():
     data_manager = ElevationDataManager()
     elevation_data = data_manager.load_elevation_data()
 
+    # Get and print the coordinates of the corners
+    corner_coordinates = data_manager.get_corner_coordinates()
+    corner_names = ["Top Left", "Top Right", "Bottom Left", "Bottom Right"]
+    for name, coords in zip(corner_names, corner_coordinates):
+        print(f"{name}: {coords}")
+
     visualizer = ElevationDataVisualizer(elevation_data)
     visualizer.visualize_elevation_data()
 
